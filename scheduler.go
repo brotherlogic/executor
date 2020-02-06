675c17513905b377839bc9891441f1315eb567a7
Simon Tucker <simon@brotherlogic@gmail>
2020-02-05 16:52:05 -0800

    Better error command. This closes #40

--- scheduler.go
+++ scheduler.go
@@ -110,7 +110,7 @@ func (s *Scheduler) run(c *rCommand) error {
 
 		s.log(fmt.Sprintf("Ran %v: %v, %v", c.base.Binary, c.output, c.erroutput))
 
-		c.err = err
+		c.err = fmt.Errorf("%v -> %v / %v", err, c.output, c.erroutput)
 	}()
 
 	return nil
