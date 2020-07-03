cdc0f791240863b2750dadce9daeac2b14b66446
Simon Tucker <brotherlogic@gmail.com>
2020-07-03 07:30:00 -0700

    Fixes executor build

--- executor.go
+++ executor.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/brotherlogic/goserver"
-	"github.com/brotherlogic/keystore/client"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"golang.org/x/net/context"
@@ -98,8 +97,6 @@ func main() {
 	server := Init()
 	server.PrepServer()
 	server.Register = server
-
-	server.GoServer.KSclient = *keystoreclient.GetClient(server.DialMaster)
 
 	err := server.RegisterServerV2("executor", false, true)
 	if err != nil {
