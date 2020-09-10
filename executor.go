0811f9935a000a47d887a92c0030cc2d166aa2d0
Simon Tucker <brotherlogic@gmail.com>
2020-09-10 06:22:52 -0700

    Fix

--- executor.go
+++ executor.go
@@ -79,7 +79,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
 	return []*pbg.State{
-		&pbg.State{Key: "nope", Value: int32(123)},
+		&pbg.State{Key: "nope", Value: int64(123)},
 	}
 }
 
