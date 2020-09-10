7571c3a3e930c01241668f4cd569854336678ade
Simon Tucker <brotherlogic@gmail.com>
2020-09-10 06:20:38 -0700

    Bounce

--- executor.go
+++ executor.go
@@ -78,7 +78,9 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{}
+	return []*pbg.State{
+		&pbg.State{Key: "nope", Value: int32(123)},
+	}
 }
 
 func (s *Server) drainQueue() {
