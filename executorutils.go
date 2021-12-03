d9b836ba22182e80c2afd6a9f5fe644290f47886
Simon Tucker <brotherlogic@gmail.com>
2021-12-02 17:53:37 -0800

    Better logging of blocking command

--- executorutils.go
+++ executorutils.go
@@ -8,10 +8,10 @@ import (
 
 func (s *Server) runQueue() {
 	for entry := range s.queue {
-		s.Log(fmt.Sprintf("THE QUEUE EXEC OUT START: %+v", entry))
+		s.Log(fmt.Sprintf("THE QUEUE EXEC OUT START: %+v => %v", entry, entry.req.Command.Binary))
 		entry.resp.Status = pb.CommandStatus_IN_PROGRESS
 		output, err := s.runExecute(entry.req)
-		s.Log(fmt.Sprintf("THE QUEUE EXEC OUT COMPLETE: %+v", entry))
+		s.Log(fmt.Sprintf("THE QUEUE EXEC OUT COMPLETE: %+v => %v", entry, entry.req.Command.Binary))
 		if err != nil {
 			entry.resp.CommandOutput = fmt.Sprintf("%v", err)
 		} else {
