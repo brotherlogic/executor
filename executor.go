55e49f26b94ed9647de80b2f218811e9fe084e2b
Simon Tucker <brotherlogic@gmail.com>
2022-08-05 08:40:26 -0700

    Fixes build. This closes #1131

--- executor.go
+++ executor.go
@@ -103,10 +103,10 @@ func main() {
 		log.SetOutput(ioutil.Discard)
 	}
 	server := Init()
-	server.PrepServer()
+	server.PrepServer("executor")
 	server.Register = server
 
-	err := server.RegisterServerV2("executor", false, true)
+	err := server.RegisterServerV2(false)
 	if err != nil {
 		return
 	}
